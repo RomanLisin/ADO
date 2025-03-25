e24f94106974a3a1d4785521b27b6c0f1fd0abfa
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-03-25 17:49:30 +1000

    InsertDirector IF NOT EXIST

--- MoviesConnector/Connector.cs
+++ MoviesConnector/Connector.cs
@@ -5,78 +5,102 @@ using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace MoviesConnector
 {
 	internal class Connector
 	{
+		static readonly int PADDING = 33;
 		readonly string CONNECTION_STRING;
 		readonly SqlConnection connection;
+		public Connector() : this(ConfigurationManager.ConnectionStrings["Movies_VPD_311"].ConnectionString)
+		{
+		//	CONNECTION_STRING =
+		//		ConfigurationManager.ConnectionStrings["Movies_VPD_311"].ConnectionString;
+		//	this.connection = new SqlConnection(CONNECTION_STRING);
+		}
 		public Connector(string connection_string)
 		{
 			this.CONNECTION_STRING = connection_string;
 			this.connection = new SqlConnection(CONNECTION_STRING);
-            Console.WriteLine(CONNECTION_STRING);
-        }
-		public void Select(string cmd)
+			Console.WriteLine(CONNECTION_STRING);
+		}
+
+		public void InsertDirector(string first_name, string last_name)
+		{
+			string condition = $"last_name = N'{last_name}' AND first_name= N'{first_name}'";
+				
+			string query = $"INSERT Directors(first_name, last_name) VALUES (N'{first_name}', N'{last_name}')";
+			string cmd = $"IF NOT EXISTS (SELECT director_id FROM Directors WHERE {condition}) BEGIN {query} END";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			
+			connection.Open();
+			command.ExecuteNonQuery();
+			connection.Close();
+		}
+		public void Select(string fields, string tables, string condition = "") // cmd)
 		{
 			int width = 0;
-			Console.WriteLine("Hello, ADO");
-			const int PADDING = 33;
-            //Console.WriteLine("------------------------------------------------------------------------------------");
-            const string CONNECTION_STRING = "Data Source=(localdb)" +
-				"\\ProjectModels;Initial Catalog=Movies_VPD_311;Integrated Security=True;" +
-				"Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;Application" +
-				"Intent=ReadWrite;MultiSubnetFailover=False";
-			Console.WriteLine(CONNECTION_STRING);
+			//Console.WriteLine("Hello, ADO");
+		//Console.WriteLine("------------------------------------------------------------------------------------");
+		//const string CONNECTION_STRING = "Data Source=(localdb)" +
+		//	"\\ProjectModels;Initial Catalog=Movies_VPD_311;Integrated Security=True;" +
+		//	"Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;Application" +
+		//	"Intent=ReadWrite;MultiSubnetFailover=False";
+		//Console.WriteLine(CONNECTION_STRING);
 
-			// 1) Создаем подключение к базе
-			SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+		// 1) Создаем подключение к базе
+		//SqlConnection connection = new SqlConnection(CONNECTION_STRING);
 
-			// 2) Создаем команду, которую хотим выполнить на Сервере:
-			//string cmd = "SELECT * FROM Movies";
-			SqlCommand command = new SqlCommand(cmd, connection);
+		// 2) Создаем команду, которую хотим выполнить на Сервере:
+		//string cmd = "SELECT * FROM Movies";
+		string cmd = $"SELECT {fields} FROM {tables}";
+		if(condition != "") cmd += $" WHERE {condition}";
 
-			// 3) Получаем результаты запроса с Сервера:
-			connection.Open();
-			SqlDataReader reader = command.ExecuteReader();
+		SqlCommand command = new SqlCommand(cmd, connection);
 
+		// 3) Получаем результаты запроса с Сервера:
+		connection.Open();
+		SqlDataReader reader = command.ExecuteReader();
+
+		Border(reader.FieldCount, "-");
+
+		// 4) Обрабатываем результаты запроса:
+		if (reader.HasRows)
+		{
 			Border(reader.FieldCount, "-");
-
-			// 4) Обрабатываем результаты запроса:
-			if (reader.HasRows)
+			for (int i = 0; i < reader.FieldCount; i++)
+				Console.Write(reader.GetName(i).ToString().PadRight(PADDING));
+			Console.WriteLine();
+			Border(reader.FieldCount, "-");
+			while (reader.Read())
 			{
-				Border(reader.FieldCount, "-");
+				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t");
 				for (int i = 0; i < reader.FieldCount; i++)
-					Console.Write(reader.GetName(i).ToString().PadRight(PADDING));
-				Console.WriteLine();
-				while (reader.Read())
 				{
-					//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t");
-					for (int i = 0; i < reader.FieldCount; i++)
-					{
-						Console.Write(reader[i].ToString().PadRight(PADDING));
-					}
-					Console.WriteLine();
+					Console.Write(reader[i].ToString().PadRight(PADDING));
 				}
+				Console.WriteLine();
 			}
+		}
 
 			//5) Закрываем поток и соединение с Сервером
 			reader.Close();
 			connection.Close();
-  
+
 			void Border(int fields_count, string symbol = "-")
 			{
 				for (int i = 0; i < fields_count; i++)
 				{
-					for(int j= 0; j<PADDING;j++)
+					for (int j = 0; j < PADDING; j++)
 					{
 
-					 Console.Write(symbol);
+						Console.Write(symbol);
 
 					}
-                    Console.WriteLine();
-                }
+				}
+				Console.WriteLine();
 			}
 		}
 	}
--- MoviesConnector/Program.cs
+++ MoviesConnector/Program.cs
@@ -11,12 +11,22 @@ namespace MoviesConnector
 		static void Main(string[] args)
 		{
 
-			const string CONNECTION_STRING = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Movies_VPD_311;Integrated Security=True;" +
-				"Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-			Connector connector = new Connector(CONNECTION_STRING);
+			//const string CONNECTION_STRING = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Movies_VPD_311;Integrated Security=True;" +
+			//	"Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			//Connector connector = new Connector(CONNECTION_STRING);
+			Connector connector  = new Connector();
 
-			connector.Select("SELECT * FROM Directors");
-			connector.Select("SELECT * FROM Movies");
+			//connector.Select("SELECT * FROM Directors");
+			//connector.Select("SELECT * FROM Movies");
+
+			//connector.Select("*", "Movies");
+			connector.InsertDirector("Matrin", "Scorsese");
+			connector.Select("*", "Directors");
+			connector.Select(
+				"title,release_date,FORMATMESSAGE(N'%s %s', first_name, last_name) AS N'Режиссер'",
+				"Movies,Directors", 
+				"director=director_id"
+				);
 
 		}
 	}
